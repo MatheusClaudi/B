397d531098d2b439ebfe0ce57a15e5f8f6d9829f
MatheusClaudi <matheus.claudino@ccc.ufcg.edu.br>
2021-05-16 15:33:24 -0300

    F

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ const path = require('path');
 
 const app = express();
 
+const PORT = process.env.PORT || 8080;
+
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/app-heroku'));
+app.use(express.static(__dirname + '/dist/vacinimin'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/app-heroku/'}),
+    res.sendFile(__dirname + '/dist/vacinimin/index.html'),
 );
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(PORT);
